e8cc6d8808d5a33f3447458e7a9cc2d534394ca1
sahil chandravanshi <sahilcdelhi@gmail.com>
2023-06-01 17:50:17 +0530

    ui buttons changed

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
 "use client";
 import Image from "next/image";
-import { createWorker } from "tesseract.js";
+// import { createWorker } from "tesseract.js";
 import Tesseract from "tesseract.js";
 import { useEffect, useState, useCallback } from "react";
 
@@ -40,14 +40,16 @@ export default function Home() {
 
 	return (
 		<main className="flex min-h-screen flex-col py-4 px-8 gap-4 font-primary bg-gray-100 w-full">
-			<label htmlFor="upload">Extract words from image. <span className="text-purple-700 hover:text-purple-900">Click here to select image!</span></label>
-			<div className="inputWrapper relative">
+			<p>
+				Extract words from image.{" "}
 				<label
 					htmlFor="upload"
-					className="inline-block py-[8px] px-[12px] cursor-pointer rounded shadow bg-purple-700 hover:bg-purple-900 text-white"
+					className="text-purple-700 hover:text-purple-900 cursor-pointer"
 				>
-					Upload Image
+					Click here to select image!
 				</label>
+			</p>
+			<div className="inputWrapper relative">
 				<input
 					type="file"
 					id="upload"
@@ -59,15 +61,27 @@ export default function Home() {
 
 			<div className="result mt-4 grid items-center justify-center sm:grid-cols-2 gap-2">
 				<div className="bg-white h-72 w-[95vw] sm:w-full sm:h-96 sm:max-h-96 p-1 overflow-y-auto flex flex-col items-center rounded shadow">
-					{selectedImage && (
-						<div className="boxImage h-96 sm:max-h-96 p-1 overflow-y-auto flex flex-col items-center">
-							<Image
-								src={URL.createObjectURL(selectedImage)}
-								alt="thumb"
-								width={1000}
-								height={500}
-							/>
+					{selectedImage == null ? (
+						<div className="w-full h-full flex justify-center items-center border-dotted border-2 border-gray-300 rounded">
+							{/* <p>Upload Here</p> */}
+							<label
+								htmlFor="upload"
+								className="inline-block py-[8px] px-[12px] cursor-pointer rounded shadow bg-purple-700 hover:bg-purple-900 text-white"
+							>
+								SELECT A FILE
+							</label>
 						</div>
+					) : (
+						selectedImage && (
+							<div className="boxImage h-96 sm:max-h-96 p-1 overflow-y-auto flex flex-col items-center">
+								<Image
+									src={URL.createObjectURL(selectedImage)}
+									alt="thumb"
+									width={1000}
+									height={500}
+								/>
+							</div>
+						)
 					)}
 				</div>
 				<div className="bg-white h-72 w-[95vw] sm:w-full sm:h-96 sm:max-h-96 p-1 overflow-y-auto flex flex-col items-center justify-center rounded shadow">
