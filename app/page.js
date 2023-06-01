8dcf4161e9578089105ab95f80272a34753d1d47
sahil chandravanshi <sahilcdelhi@gmail.com>
2023-06-01 21:49:01 +0530

    upload button styled

--- app/page.js
+++ app/page.js
@@ -3,6 +3,8 @@ import Image from "next/image";
 // import { createWorker } from "tesseract.js";
 import Tesseract from "tesseract.js";
 import { useEffect, useState, useCallback } from "react";
+import { FiUpload } from 'react-icons/fi';
+import { FaFileUpload } from 'react-icons/fa';
 
 export default function Home() {
 	const [selectedImage, setSelectedImage] = useState(null);
@@ -39,7 +41,7 @@ export default function Home() {
 	};
 
 	return (
-		<main className="flex min-h-screen flex-col py-4 px-8 gap-4 font-primary bg-gray-100 w-full">
+		<main className="flex min-h-screen flex-col py-4 px-8 gap-2 font-primary bg-gray-100 w-full">
 			<p>
 				Extract words from image.{" "}
 				<label
@@ -59,21 +61,21 @@ export default function Home() {
 				/>
 			</div>
 
-			<div className="result mt-4 grid items-center justify-center sm:grid-cols-2 gap-2">
+			<div className="result mt-2 grid items-center justify-center sm:grid-cols-2 gap-2">
 				<div className="bg-white h-72 w-[95vw] sm:w-full sm:h-96 sm:max-h-96 p-1 overflow-y-auto flex flex-col items-center rounded shadow">
 					{selectedImage == null ? (
 						<div className="w-full h-full flex justify-center items-center border-dotted border-2 border-gray-300 rounded">
 							{/* <p>Upload Here</p> */}
 							<label
 								htmlFor="upload"
-								className="inline-block py-[8px] px-[12px] cursor-pointer rounded shadow bg-purple-700 hover:bg-purple-900 text-white"
+								className="py-4 px-8 cursor-pointer rounded border-[1.5px] border-purple-300 hover:border-purple-600 hover:bg-purple-100 text-purple-500 hover:text-purple-900 flex flex-col gap-2 items-center justify-center text-sm"
 							>
-								SELECT A FILE
+								<FaFileUpload className="text-3xl text-purple-600 hover:text-purple-900"/>Select Image
 							</label>
 						</div>
 					) : (
 						selectedImage && (
-							<div className="boxImage h-96 sm:max-h-96 p-1 overflow-y-auto flex flex-col items-center">
+							<div className="boxImage h-96 sm:max-h-96 overflow-y-auto flex flex-col items-center rounded">
 								<Image
 									src={URL.createObjectURL(selectedImage)}
 									alt="thumb"
@@ -100,19 +102,6 @@ export default function Home() {
 						)
 					)}
 				</div>
-				{/* {progress < 100 && progress > 0 && (
-					<div>
-						<div className="progress-label">Progress ({progress}%)</div>
-						<div className="progress-bar">
-							<div className="progress" style={{ width: `${progress}%` }}></div>
-						</div>
-					</div>
-				)}
-				{textResult && (
-					<div className="box-p font-codeFont bg-gray-300 max-h-96 overflow-auto p-4 font-light">
-						<p>{textResult}</p>
-					</div>
-				)} */}
 			</div>
 		</main>
 	);
